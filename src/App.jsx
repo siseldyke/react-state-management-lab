eb64bb1ffef09c4f677c7760ecb5a7b77d382546
siseldyke <siseldyke@gmail.com>
2024-11-01 15:32:32 -0400

    on click function working

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,107 @@
-// src/App.jsx
+
 import { useState } from 'react'
 import './App.css'
 import ZombieFighterList from './components/ZombieFighterList/ZombieFighterList.jsx'
 
 const App = () => {
+  const zombieFightersArray =[{
+    name: 'Survivor',
+    price: 12,
+    strength: 6,
+    agility: 4,
+    img: 'https://via.placeholder.com/150/92c952',
+  },
+  {
+    name: 'Scavenger',
+    price: 10,
+    strength: 5,
+    agility: 5,
+    img: 'https://via.placeholder.com/150/771796',
+  },
+  {
+    name: 'Shadow',
+    price: 18,
+    strength: 7,
+    agility: 8,
+    img: 'https://via.placeholder.com/150/24f355',
+  },
+  {
+    name: 'Tracker',
+    price: 14,
+    strength: 7,
+    agility: 6,
+    img: 'https://via.placeholder.com/150/d32776',
+  },
+  {
+    name: 'Sharpshooter',
+    price: 20,
+    strength: 6,
+    agility: 8,
+    img: 'https://via.placeholder.com/150/1ee8a4',
+  },
+  {
+    name: 'Medic',
+    price: 15,
+    strength: 5,
+    agility: 7,
+    img: 'https://via.placeholder.com/150/66b7d2',
+  },
+  {
+    name: 'Engineer',
+    price: 16,
+    strength: 6,
+    agility: 5,
+    img: 'https://via.placeholder.com/150/56acb2',
+  },
+  {
+    name: 'Brawler',
+    price: 11,
+    strength: 8,
+    agility: 3,
+    img: 'https://via.placeholder.com/150/8985dc',
+  },
+  {
+    name: 'Infiltrator',
+    price: 17,
+    strength: 5,
+    agility: 9,
+    img: 'https://via.placeholder.com/150/392537',
+  },
+  {
+    name: 'Leader',
+    price: 22,
+    strength: 7,
+    agility: 6,
+    img: 'https://via.placeholder.com/150/602b9e',
+  }]
+
 
     const [team, setTeam] = useState([])
     const [money, setMoney] = useState(100)
-    const [zombieFighters, setZombieFighters] = useState( [
-      {
-        name: 'Survivor',
-        price: 12,
-        strength: 6,
-        agility: 4,
-        img: 'https://via.placeholder.com/150/92c952',
-      },
-      {
-        name: 'Scavenger',
-        price: 10,
-        strength: 5,
-        agility: 5,
-        img: 'https://via.placeholder.com/150/771796',
-      },
-      {
-        name: 'Shadow',
-        price: 18,
-        strength: 7,
-        agility: 8,
-        img: 'https://via.placeholder.com/150/24f355',
-      },
-      {
-        name: 'Tracker',
-        price: 14,
-        strength: 7,
-        agility: 6,
-        img: 'https://via.placeholder.com/150/d32776',
-      },
-      {
-        name: 'Sharpshooter',
-        price: 20,
-        strength: 6,
-        agility: 8,
-        img: 'https://via.placeholder.com/150/1ee8a4',
-      },
-      {
-        name: 'Medic',
-        price: 15,
-        strength: 5,
-        agility: 7,
-        img: 'https://via.placeholder.com/150/66b7d2',
-      },
-      {
-        name: 'Engineer',
-        price: 16,
-        strength: 6,
-        agility: 5,
-        img: 'https://via.placeholder.com/150/56acb2',
-      },
-      {
-        name: 'Brawler',
-        price: 11,
-        strength: 8,
-        agility: 3,
-        img: 'https://via.placeholder.com/150/8985dc',
-      },
-      {
-        name: 'Infiltrator',
-        price: 17,
-        strength: 5,
-        agility: 9,
-        img: 'https://via.placeholder.com/150/392537',
-      },
-      {
-        name: 'Leader',
-        price: 22,
-        strength: 7,
-        agility: 6,
-        img: 'https://via.placeholder.com/150/602b9e',
-      },
-    ]);
+    const [totalStrength, setTotalStrength] =useState(0)
+    const [totalAgility, setTotalAgility] =useState(0)
+    const [zombieFighters, setZombieFighters] = useState(zombieFightersArray);
    
+    const handleAddFighter =(fighter) => {
+      const updatedTeam = [...team, fighter]
+      setTeam(updatedTeam)
+    }
+
   return (
     <>
-    <h1>Zombie Fighters</h1>
-    <h2>Money: {money}</h2>
-    <h2>Team Strength:</h2>
-    <h2>Team Agility:</h2>
-    <h2>Team</h2>
-    <h2>Fighters</h2>
-    <ul>
-    {zombieFighters.map((zombieFighter) => (
-      <ZombieFighterList
-      key = {zombieFighter.id}
-      zombieFighter ={zombieFighter}
-      
+      <ZombieFighterList 
+        zombieFighters={zombieFighters}
+        handleAddFighter={handleAddFighter}
       />
+    </>
       
-    ))}
+    )
     
     
-    </ul>
+   
 
-  </>
-  );
+
+  
 }
 
 export default App
--- src/components/ZombieFighterList/ZombieFighterList.jsx
+++ src/components/ZombieFighterList/ZombieFighterList.jsx
@@ -1,21 +1,19 @@
 // import handleAddFighter from './src/App.jsx'
 
 const ZombieFighterList = (props)=> {
-    const {zombieFighter} = props
-    const handleAddFighter  =(addZombieFighter) =>{
-        const newFighterArray= [...team, addZombieFighter]
-        setZombieFighters(newFighterArray)
-      }
+    
+   
+      
 return (
     
     <ul>
-        <li><img src ={zombieFighter.img} alt ={zombieFighter.name}/></li>
-        <li>{zombieFighter.name}</li>
-        <li>Price: {zombieFighter.price}</li>
-        <li>Strength: {zombieFighter.strength}</li>
-        <li>Agility: {zombieFighter.agility}</li>
         
-        <li><button onClick={() => handleAddFighter({zombieFighter})}>Add</button></li>
+       {props.zombieFighters.map((zombieFighter)=>(
+        <div>
+            <h1>{zombieFighter.name}</h1>
+            <button onClick={()=>{props.handleAddFighter(zombieFighter)}}>Add Fighter</button>
+        </div>
+       ))}
     </ul>
 
 )
